28fb6ec8584e8114840269379660d6c08cf1a3b5
Ingo <roessner@infai.org>
2020-01-13 14:22:41 +0100

    fix KafkaIncidentMessage.TenantId bson

--- lib/messages/kafka.go
+++ lib/messages/kafka.go
@@ -27,5 +27,5 @@ type KafkaIncidentMessage struct {
 	WorkerId            string    `json:"worker_id" bson:"worker_id"`
 	ErrorMessage        string    `json:"error_message" bson:"error_message"`
 	Time                time.Time `json:"time" bson:"time"`
-	TenantId            string    `json:"tenant_id"`
+	TenantId            string    `json:"tenant_id" bson:"tenant_id"`
 }
